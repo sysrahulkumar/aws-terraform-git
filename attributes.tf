fa2c185ac893f2d8b5fee526d0e3fcb541b66c9f
sysrahulkumar <134920184+sysrahulkumar@users.noreply.github.com>
2023-05-30 13:24:11 +0530

    first commit

---
+++ attributes.tf
@@ -0,0 +1,46 @@
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "= 5.0"
+#    }
+#  }
+#}
+
+#provider "aws"{
+# region = "us-east-1"
+# access_key = "AKIA246BBFWRNCS3GOIQ"
+# secret_key = "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"
+#}
+
+resource "aws_eip" "lb" {
+vpc = true
+}
+
+output "eip" {
+value = aws_eip.lb.public_ip
+}
+
+#resource "aws_s3_bucket" "mys3bucket" {
+#bucket = "training-day-002"
+#}
+
+#output "mys3bucket" {
+#value = aws_s3_bucket.mys3.bucket_domain_name
+#}
+
+resource "aws_instance" "myec2" {
+ami = "ami-0715c1897453cabd1"
+instance_type = "t2.micro"
+}
+resource "aws_eip" "lb" {
+vpc = true
+}
+resource "aws_eip_association" "eip_assoc" {
+instance_id = aws_instance.myec2.id
+allocation_id = aws_eip.lb.id
+}
+
+provider "github"{
+token = "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"
+}
