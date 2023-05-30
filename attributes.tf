a16d8e369116ccfee24436f4fef73a4d0c232f32
sysrahulkumar <134920184+sysrahulkumar@users.noreply.github.com>
2023-05-30 13:32:01 +0530

    first commit

--- attributes.tf
+++ attributes.tf
@@ -13,7 +13,7 @@
 # secret_key = "+x40Hq+ErG75sioFuGzHmvdzP0XmkZM5AdmZlaCT"
 #}
 
-resource "aws_eip" "lb" {
+resource "aws_eip" "lb1" {
 vpc = true
 }
 
@@ -33,14 +33,14 @@ resource "aws_instance" "myec2" {
 ami = "ami-0715c1897453cabd1"
 instance_type = "t2.micro"
 }
-resource "aws_eip" "lb" {
+resource "aws_eip" "lb1" {
 vpc = true
 }
 resource "aws_eip_association" "eip_assoc" {
 instance_id = aws_instance.myec2.id
-allocation_id = aws_eip.lb.id
+allocation_id = aws_eip.lb1
 }
 
-provider "github"{
-token = "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"
-}
+#provider "github"{
+#token = "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"
+#}
